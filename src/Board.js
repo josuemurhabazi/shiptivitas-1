e813a7186daadc27bba896dcba9689d10942b5d4
Josué Murhabazi <josuemurhabazi@gmail.com>
2020-12-14 02:52:13 +0300

    use of Dragula to move shipping requests

--- src/Board.js
+++ src/Board.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Dragula from 'dragula';
+import Dragula from 'react-dragula';
 import 'dragula/dist/dragula.css';
 import Swimlane from './Swimlane';
 import './Board.css';
@@ -73,6 +73,55 @@ export default class Board extends React.Component {
           </div>
         </div>
       </div>
+       
     );
+   
   }
+
+      
+componentDidMount() {
+  this.drake = Dragula([
+    this.swimlanes.backlog.current,
+    this.swimlanes.inProgress.current,
+    this.swimlanes.complete.current,
+  ]);
+  this.drake.on('drop', (el, target, source, sibling) => this.updateClient(el, target, source, sibling));
+}
+
+componentWillUnmount() {
+  this.drake.remove();
+}
+
+updateClient(el, target, _, sibling) {
+  this.drake.cancel(true);
+
+  let targetSwimlane = 'backlog';
+  if (target === this.swimlanes.inProgress.current) {
+    targetSwimlane = 'in-progress';
+  } else if (target === this.swimlanes.complete.current) {
+    targetSwimlane = 'complete';
+  }
+
+  const clientsList = [
+    ...this.state.clients.backlog,
+    ...this.state.clients.inProgress,
+    ...this.state.clients.complete,
+  ];
+  const clientThatMoved = clientsList.find(client => client.id === el.dataset.id);
+  const clientThatMovedClone = {
+    ...clientThatMoved,
+    status: targetSwimlane,
+  };
+
+  const updatedClients = clientsList.filter(client => client.id !== clientThatMovedClone.id);
+  const index = updatedClients.findIndex(client => sibling && client.id === sibling.dataset.id);
+  updatedClients.splice(index === -1 ? updatedClients.length : index , 0, clientThatMovedClone);
+  this.setState({
+    clients: {
+      backlog: updatedClients.filter(client => !client.status || client.status === 'backlog'),
+      inProgress: updatedClients.filter(client => client.status && client.status === 'in-progress'),
+      complete: updatedClients.filter(client => client.status && client.status === 'complete'),
+    }
+  });
+}
 }
